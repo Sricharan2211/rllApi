7d8e285716ffd323d34d9913521df1569bbeb8cd
Sricharan2211 <tudugani.charan@mphasis.com>
2024-01-30 14:24:07 +0530

    API Changes

---
+++ InsuranceManagementSystem/InsuranceManagementSystem/WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DAL;
+
+namespace WebAPI.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        private readonly InsuranceDbContext _db = new InsuranceDbContext();
+
+        public IQueryable<Customer> GetCustomers()
+        {
+            return _db.Customers;
+        }
+
+        [ResponseType(typeof(Customer))]
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = _db.Customers.Find(id);
+
+            if (customer == null)
+                return NotFound();
+
+            return Ok(customer);
+        }
+
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (id != customer.Id)
+                return BadRequest();
+
+            _db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                    return NotFound();
+
+                throw;
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [ResponseType(typeof(Customer))]
+        public IHttpActionResult PostCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _db.Customers.Add(customer);
+            _db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = customer.Id }, customer);
+        }
+
+        [ResponseType(typeof(Customer))]
+        public IHttpActionResult DeleteCustomer(int id)
+        {
+            var customer = _db.Customers.Find(id);
+
+            if (customer == null)
+                return NotFound();
+
+            _db.Customers.Remove(customer);
+            _db.SaveChanges();
+
+            return Ok(customer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _db.Customers.Count(e => e.Id == id) > 0;
+        }
+    }
+}
